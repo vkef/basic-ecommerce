968eb32562915d5a197ff546d5376f7d8e5be1a4
Vasilieos Kefalas <kef.vasileios@outlook.com>
2021-01-05 16:55:08 +0200

    Invervention Image Package

--- config/app.php
+++ config/app.php
@@ -161,11 +161,16 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        
+
+
 
         /*
          * Package Service Providers...
          */
 
+        Intervention\Image\ImageServiceProvider::class,
+        
         /*
          * Application Service Providers...
          */
@@ -228,6 +233,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Image' => Intervention\Image\Facades\Image::class,
+
+
 
     ],
 
